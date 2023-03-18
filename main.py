f383d26421e2f276e2ba9bfda25c4354e4adcbfd
DataMonarch <toghrultahirov2@gmail.com>
2023-03-18 23:07:01 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from input_parser import TPInput
+from landscape import Landscape
+from backtracking import solve
+import os
+import time
+
+
+if __name__ == "__main__":
+    input_name = 'tilesproblem_1326658928646700.txt'
+    file = os.path.join('inputs', input_name)
+    
+    tile_input = TPInput(file)
+    landscape = Landscape(tile_input)
+    
+    print(landscape)
+    print(landscape.targets)
+    
+    start_time = time.time()
+    solve(landscape, 0, 0)
+    print(landscape)
+    print(landscape.count_colors(landscape.bushes))
+    print(landscape.print_output())
+    print("CSP performed in %.3f seconds" % (time.time() - start_time))
